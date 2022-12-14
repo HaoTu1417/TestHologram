4a0c54bad17367fa42559541d64757d0a6309030
Hieu.Tu <105291682+snipZ09@users.noreply.github.com>
2022-12-14 16:07:23 +0700

    fix(sonar): move class into namespace

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour
+namespace DiTichCoDoHue
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Testing : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
