e598e837fa89b9eb81bb47f589f492b57e812aca
khang2508 <khang.nguyen@alta.com.vn>
2022-12-15 10:51:36 +0700

    update(audio): set camera base on two V2

--- Assets/Scripts/Camera/PreviewCamera.cs
+++ Assets/Scripts/Camera/PreviewCamera.cs
@@ -9,45 +9,60 @@ namespace DiTichCoDoHue
     {
         [SerializeField] private Camera previewCam;
         [SerializeField] private Object anRong;
-
-
-        void Start()
-        {
-
-        }
-
+        [SerializeField] private Transform point1;
+        [SerializeField] private Transform point2;
+        private Vector2 pos1;
+        private Vector2 pos2;
+        
         void Update()
         {
             ChangeCamView();
         }
-
-        private void Angle(int fov, Rect rect, Vector3 transform)
+        private void Angle(int fov, Rect rect, Vector2 pos1, Vector2 pos2) // Set Angle for Preview Camera.
         {
-            //previewCam.fieldOfView = 33;
-            //previewCam.rect = new Rect(0f, 0f, 0.5f, 1f);
-            //previewCam.transform.position = new Vector3(-0.3f, -0.05f, 1.5f);
             previewCam.fieldOfView = fov;
             previewCam.rect = new Rect(rect);
-            previewCam.transform.localPosition = transform;
+            point1.localPosition = pos1;
+            point2.localPosition = pos2;
+            transform.position = (point1.position + point2.position) / 2;
+            transform.position -= transform.forward * 7; ;
         }
-
-        public void ChangeCamView()
+        private void CheckPos() // Check Pos when receive data from other script.
+        {
+            //pos1 = data1;
+            //pos2 = data2;
+            pos1.x = Mathf.Clamp(pos1.x, -0.5f, 0.5f);
+            pos1.y = Mathf.Clamp(pos1.y, -0.5f, 0.5f);
+            pos2.x = Mathf.Clamp(pos2.x, -0.5f, 0.5f);
+            pos2.y = Mathf.Clamp(pos2.y, -0.5f, 0.5f);
+        }
+        private void ChangeCamView() // Change Preview Camera Angle.
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Angle(33, new Rect(0f, 0f, 0.5f, 1f), new Vector3(-0.3f, -0.05f, 1.5f));
+                pos1 = new Vector2(-0.125f, 0.5f);
+                pos2 = new Vector2(-0.5f, -0.5f);
+                CheckPos();
+                Angle(33, new Rect(0f, 0f, 0.5f, 1f), pos1, pos2);
             }
 
             if (Input.GetKeyDown(KeyCode.W))
             {
-                Angle(33, new Rect(0f, 0f, 0.5f, 1f), new Vector3(-0.13f, -0.132f, 1.125f));
+                pos1 = new Vector2(0.125f, 0.5f);
+                pos2 = new Vector2(-0.125f, -0.5f);
+                CheckPos();
+                Angle(33, new Rect(0f, 0f, 0.5f, 1f), pos1, pos2); 
             }
 
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                Angle(37, new Rect(0f, 0f, 0.5f, 1f), new Vector3(0.415f, -0.048f, 1.19f));
+                pos1 = new Vector2(0.5f, 0.5f);
+                pos2 = new Vector2(0.125f, -0.5f);
+                CheckPos();
+                Angle(37, new Rect(0f, 0f, 0.5f, 1f), pos1, pos2);
             }
         }
     }
 }
+
 
